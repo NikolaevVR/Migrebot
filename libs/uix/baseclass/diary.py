1b932059661cd1bcf8a3cd759ce72723c9f6ea0f
GavrilinYP <Gavrilin-Yuriy@mail.ru>
2020-07-26 23:16:26 +0300

    Прототип добовления записи
    Прототип добовления записи

---
+++ libs/uix/baseclass/diary.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# This file created with KivyCreatorProject
+# <https://github.com/HeaTTheatR/KivyCreatorProgect
+#
+# Copyright © 2017 Easy
+#
+# For suggestions and questions:
+# <kivydevelopment@gmail.com>
+# 
+# LICENSE: MIT
+
+
+
+from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
+
+class Diary(Screen):
+    mail = ObjectProperty()
+    theme = ObjectProperty()
+
+
